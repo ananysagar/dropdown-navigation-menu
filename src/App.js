8c4e5780d2c1cde97265898bef1aca8ce7dfd536
ananysagar <sagaranany577@gmail.com>
2025-03-08 16:32:57 +0530

    quick fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Dropdown from "./Dropdown";
-import Dropdown2 from "./Dropdown2";
 
 const App = () => {
   const options = ["Option 1", "Option 2", "Option 3"];
@@ -13,7 +12,6 @@ const App = () => {
     <div style={{ padding: "50px" }}>
       <h2>Custom Dropdown</h2>
       <Dropdown options={options} onSelect={handleSelect} />
-      {/* <Dropdown2 options={options} onSelect={handleSelect}/> */}
     </div>
   );
 };
